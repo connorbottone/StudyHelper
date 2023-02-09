44a79614440406fbd9b32592857988e1e455f586
connorbottone <42557300+connorbottone@users.noreply.github.com>
2023-02-09 14:37:47 -0500

    created pages and nav bar to jump from pages to page all functionality works

---
+++ my-app/src/components/SignupLogin.js
@@ -0,0 +1,9 @@
+const SignupLogin = () => {
+    return (
+        <div>
+        <h1>SignupLogin</h1>
+        
+        </div>
+    );
+    }
+    export default SignupLogin;
---
+++ my-app/src/components/index.js
@@ -0,0 +1,8 @@
+import Header from "./Header";
+
+import Homepage from "./Homepage";
+import Courses from "./Courses";
+import SingleCourse from "./SingleCourse";
+import SignupLogin from "./SignupLogin";
+
+export { Header,  Homepage, Courses, SingleCourse, SignupLogin }
