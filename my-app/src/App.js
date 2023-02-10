0cab90296e8d3cb6f47e27aad889a318ef2247c8
connorbottone <42557300+connorbottone@users.noreply.github.com>
2023-02-10 13:33:45 -0500

    jwt boilerplate

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import {Courses , Homepage,CreateQuiz, SignupLogin, SingleCourse, Header} from '../src/components';
-
+import SingleQuiz from './components/SingleQuiz';
+import  '../src/assets/style.css';
 function App() {
   const [currentPage, setCurrentPage] = useState("Homepage");
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -10,8 +11,9 @@ function App() {
     <div >{currentPage === "Homepage" && <Homepage setCurrentPage={setCurrentPage} isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>}
   {currentPage === "SignupLogin" && <SignupLogin />}
   {currentPage === "Courses" && <Courses currentPage={currentPage} setCurrentPage={setCurrentPage} isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>}
-  {currentPage === "SingleCourse" && <SingleCourse/>}
+  {currentPage === "SingleCourse" && <SingleCourse currentPage={currentPage} setCurrentPage={setCurrentPage} isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>}
   {currentPage === "CreateQuiz" && <CreateQuiz/>}
+  {currentPage === "SingleQuiz" && <SingleQuiz/>}
   </div>
   <br></br>
   {/* <Footer setCurrentPage={setCurrentPage} /> */}
--- my-app/src/components/SingleCourse.js
+++ my-app/src/components/SingleCourse.js
@@ -1,7 +1,15 @@
-const SingleCourse = () => {
+const SingleCourse = ({currentPage, setCurrentPage}) => {
     return (
-        <div>
-            <h1>this is the Single Course</h1>
+        
+            <div class="card text-center  border border-primary mt-4">
+    <div class="card-header">
+        English
+    </div>
+    <div class="card-body">
+        <h5 class="card-title"> english quiz</h5>
+        <p class="card-text">blah blah blah about us and the quizes</p>
+        <button onClick={()=> setCurrentPage('SingleQuiz')} class="btn btn-primary">Begin Learning</button>
+    </div>
         </div>
     );
 }
