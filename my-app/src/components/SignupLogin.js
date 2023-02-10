56f8cc422c53b99596e1c30550f54f4cdcb7e856
David Yu <dyu4848@gmail.com>
2023-02-10 13:03:37 -0500

    login page form with functional button

--- my-app/src/components/SignupLogin.js
+++ my-app/src/components/SignupLogin.js
@@ -14,44 +14,27 @@ const SignupLogin = ({
   return (
     <div class="row g-3">
   <div class="col-md-6">
-    <label for="inputEmail4" class="form-label">Email</label>
-    <input type="email" class="form-control" id="inputEmail4"></input>
+    <label for="first_name" class="form-label">First Name</label>
+    <input type="text" class="form-control" id="first_name"></input>
   </div>
   <div class="col-md-6">
-    <label for="inputPassword4" class="form-label">Password</label>
-    <input type="password" class="form-control" id="inputPassword4"></input>
+    <label for="last_name" class="form-label">Last Name</label>
+    <input type="text" class="form-control" id="last_name"></input>
   </div>
   <div class="col-12">
     <label for="inputEmail4" class="form-label">Email</label>
     <input type="email" class="form-control" id="inputEmail4" placeholder="John@test.com"></input>
   </div>
   <div class="col-12">
-    <label for="inputAddress2" class="form-label">Address 2</label>
-    <input type="password" class="form-control" id="inputPassword4" placeholder="Apartment, studio, or floor"></input>
+    <label for="inputAddress2" class="form-label">Password</label>
+    <input type="password" class="form-control" id="inputPassword4" placeholder="Password123"></input>
   </div>
   <div class="col-md-6">
     <label for="inputCity" class="form-label">City</label>
     <input type="text" class="form-control" id="inputCity"></input>
   </div>
-  <div class="col-md-4">
-    <label for="inputState" class="form-label">State</label>
-    <select id="inputState" class="form-select">
-      <option selected>Choose...</option>
-      <option>...</option>
-    </select>
-  </div>
-  <div class="col-md-2">
-    <label for="inputZip" class="form-label">Zip</label>
-    <input type="text" class="form-control" id="inputZip"></input>
-  </div>
-  <div class="col-12">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck"></input>
-      <label class="form-check-label" for="gridCheck">
-        Check me out
-      </label>
-    </div>
-  </div>
+
+
   <div class="col-12">
     <button onClick={() => handleLogin("Homepage")} type="submit" class="btn btn-primary">Submit</button>
   </div>
