cf465140ebbf83ce4c170af96eac6c0dd73d6b98
connorbottone <42557300+connorbottone@users.noreply.github.com>
2023-02-09 23:16:23 -0500

    modified header.js to handle lougout

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -18,7 +18,10 @@ const Header = ({ currentPage, setCurrentPage, isLoggedIn,setIsLoggedIn}) => {
                         id="nav-page"
                         className={currentPage === page ? "navActive" : "nav"}
                         onClick={() => {
-                            if (page === "SignupLogin") {
+                            if (page === "SignupLogin" && isLoggedIn) {
+                                handleLogout();
+                            }
+                           else if (page === "SignupLogin") {
                                 setIsLoggedIn(true); //this needs to be implementd into the sign up form here to test it
                             } else {
                                 setCurrentPage(page);
