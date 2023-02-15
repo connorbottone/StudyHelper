effff95787ee9003d70092dd407fa831f78ab914
connorbottone <42557300+connorbottone@users.noreply.github.com>
2023-02-14 21:30:14 -0500

    homepage styling

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useQuery } from '@apollo/client';
 import { Link } from 'react-router-dom';
 import test from '../images/test.jpg'
-
+import create from '../images/create.png'
 const Home = () => {
   return (
     <div className="homepage" style={{ display: "flex", flexDirection: "row", height: "80vh" }}>
@@ -13,10 +13,14 @@ const Home = () => {
         <div className="card-body flex-grow-1">
           <h5 className="card-title">Explore content</h5>
           <p className="card-text flex-grow-1">
-            blah blah blah about us and the quizes
+            Quizes from grade 1-3
           </p>
+          
+          <p>  🎓English 📊Math 📜History </p>
           <div className="mt-auto">
-           <img  id="homepageimg1"src={test} alt="Your Image" className="profile-pic" /> 
+           <img  id="homepageimg1"src={test} alt="Brain growing " className="profile-pic" /> 
+           <br></br>
+           <br></br>
            <Link className="btn btn-lg btn-info m-2" to="/Courses">
               Explore content
             </Link>
@@ -33,9 +37,13 @@ const Home = () => {
         <div className="card-body flex-grow-1">
           <h5 className="card-title">Make a quiz</h5>
           <p className="card-text flex-grow-1">
-            Talk about creating a teacher account and making your own quizzes
+            Create a quiz for your students
+          </p>
+          <p>
+            📝Simply follow the steps to create your quiz!
           </p>
           <div className="mt-auto">
+          <img  id="homepageimg2"src={create} alt="Pencil on paper" className="profile-pic" /> 
             <Link className="btn btn-lg btn-info m-2" to="/CreateQuiz">
               CreateQuiz
             </Link>
